32deb9e85f8a0eedd432be26e721e82c26c5f1df
hamed mn <hamedmoshfeghi7@gmail.com>
2023-02-23 15:15:28 +0330

    projects section from index page moved to partial view Projects.

---
+++ DevCard_MVC/Models/Project.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DevCard_MVC.Models
+{
+    public class Project
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+    }
+
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title  { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class IndexModel
+    {
+        public List<Project> Projects { get; set; }
+        public List<Article> Articles{ get; set; }
+
+    }
+
+}
